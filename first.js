355d49205e35839fef22a07a49ccb04e32b2a5eb
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-01-10 12:21:37 +0600

    randomNum.js added

--- first.js
+++
@@ -1,8 +0,0 @@
-var statement = "Hello, I am RAC. I am a CSE student. Currently I am working with ARC.";
-
-var segments = statement.split('I');
-
-for(i in segments){
-    console.log(segments[i]);
-}
-
---
+++ randomNum.js
@@ -0,0 +1,4 @@
+var randomNum = Math.random() * 6;
+var dice = Math.round(randomNum);
+
+console.log(dice);
