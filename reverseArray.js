224ac1ecd89570f92da2b6e4c91d7dbab15b2242
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-01-23 11:55:11 +0600

    spread.js added

---
+++ reverseArray.js
@@ -0,0 +1,5 @@
+var arr = [2, 3, 6, 7, 8];
+
+var rev = arr.reverse();
+
+console.log(rev);
---
+++ spread.js
@@ -0,0 +1,6 @@
+const friends = ['Omur', 'Ashfak', 'Nafiz', 'Shamim'];
+const seniors = ['Sazzad', 'Yasir', 'Fahim'];
+
+const all = [...friends, ...seniors];
+
+console.log(all);
