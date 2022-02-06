b7bb53cdf9a23c7eec527159418e73d00fe17e1c
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-02-06 14:53:54 +0600

    ternaryOperator.js modified

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -1,3 +1,4 @@
+// ***** Ternary operator instead of if-else *****
 const isActive = true;
 
 // If isActive == true, then call showUser() and if false then call hideUser() 
@@ -10,3 +11,13 @@ const showUser = () => {
 const showUser = () => {
     console.log('User is inactive');
 }
+
+
+// ***** Shortcut for only if condition; means, 'if so and so is true, then do that' ***** 
+const legitimate = false;
+const authenticate = legitimate && login() // this will go to the both parts of this statement
+
+
+// ***** Shortcut for only if not condition; means, 'if so and so is false, then do that' ***** 
+const remainingBalance = true;
+const logout = remainingBalance || logout() // this will execute logout() only if remainingBalance is false
